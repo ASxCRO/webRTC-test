1e3a2cddc6aae66b81111002415e9f547459af07
antonio.supan <antonio.supan@fina.hr>
2022-05-18 10:28:57 +0200

    local + remote stream, peer connections

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 let localStream;
 let remoteStream;
+let peerConnection;
+
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.1.google.com:19302", "stun:stun2.1.google.com:19302"],
+    },
+  ],
+};
 
 let init = async () => {
   localStream = await navigator.mediaDevices.getUserMedia({
@@ -7,7 +16,37 @@ let init = async () => {
     audio: false,
   });
 
-  document.getElementById("user-1").scrObject = localStream;
+  document.getElementById("user-1").srcObject = localStream;
+
+  createOffer();
+};
+
+let createOffer = async () => {
+  peerConnection = new RTCPeerConnection(servers);
+
+  remoteStream = new MediaStream();
+  document.getElementById("user-2").srcObject = remoteStream;
+
+  localStream.getTracks().forEach((t) => {
+    peerConnection.addTrack(t, localStream);
+  });
+
+  peerConnection.ontrack = (event) => {
+    event.streams[0].getTracks.forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  peerConnection.onicecandidate = async (event) => {
+    if (event.candidate) {
+      console.log("New ICE candidate", event.candidate);
+    }
+  };
+
+  let offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+
+  console.log(offer);
 };
 
 init();
